07f18fdc05424a22a0ef1ef59326adaa4ddaba93
Stefan B <stefanbolcs@gmail.com>
2020-11-30 21:41:06 +0100

    adding hobbies  and creating own validator for usernames

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component ,OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +9,44 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-          'username' : new FormControl(null, Validators.required),
+          'username' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
           'email': new FormControl(null,[Validators.required, Validators.email])
     }),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  //*ngFor="let hobbyControl of controls; let i = index"
+  // get controls() {
+  //   return (this.signupForm.get('hobbies') as FormArray).controls;
+  // }
 
   onSubmit(){
     console.log(this.signupForm);
   }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+      (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  //writing own validator here
+  forbiddenNames(control: FormControl):{[s: string]:boolean}{
+    //checks if its part of array, will return -1 if it is not part
+    if(this.forbiddenUsernames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
 }
